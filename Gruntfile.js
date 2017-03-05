a810858743f51c003bf64043ad3045c19a8ca574
simonihmig <ihmig@kaliber5.de>
2017-03-05 18:35:23 +0100

    Addon section

--- Gruntfile.js
+++ Gruntfile.js
@@ -122,7 +122,7 @@ module.exports = function(grunt) {
 				tasks: 'js'
 			},
 			theme: {
-				files: [ 'css/theme/source/*.scss', 'css/theme/template/*.scss' ],
+				files: [ 'css/theme/source/**/*.scss', 'css/theme/template/*.scss' ],
 				tasks: 'css-themes'
 			},
 			css: {
